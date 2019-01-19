2c91e4d82fd23ee7e177a3933717bb815c13eb52
Quabynah-Codelabs <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-01-19 17:52:47 +0000

    Update MainActivity.kt

--- coupon-generator/app/src/main/java/io/bilghazy/coupongenerator/MainActivity.kt
+++ coupon-generator/app/src/main/java/io/bilghazy/coupongenerator/MainActivity.kt
@@ -3,10 +3,17 @@ package io.bilghazy.coupongenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * This is the main activity
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+
 }
